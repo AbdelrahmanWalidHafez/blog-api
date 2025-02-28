fe72ec8595953fc917744d5a582f35393a090fb1
AbdelrahmanWalidHafez <abdelrahman.walid.804@gmail.com>
2025-02-28 02:23:36 +0200

    Update CsrfCookieFilter.java

--- blog/src/main/java/com/abdelrahman/blog/domain/filter/CsrfCookieFilter.java
+++ blog/src/main/java/com/abdelrahman/blog/domain/filter/CsrfCookieFilter.java
@@ -12,17 +12,13 @@ import java.io.IOException;
 
 public class CsrfCookieFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException,IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        // Render the token value to a cookie by causing the deferred token to be loaded
+        csrfToken.getToken(); 
+    
+        
 
-        if (csrfToken != null) {
-            Cookie csrfCookie = new Cookie("XSRF-TOKEN", csrfToken.getToken());
-            csrfCookie.setPath("/");
-            csrfCookie.setHttpOnly(false);
-            csrfCookie.setSecure(true);
-            csrfCookie.setMaxAge(3600);
-            response.addCookie(csrfCookie);
-        }
 
         filterChain.doFilter(request, response);
     }
